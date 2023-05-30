05963743afcaac5231ef001fd80e449462bfbb56
Niklas <niklas.kinze@gmail.com>
2023-05-30 16:18:21 +0200

    uploaded tessdata value

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -13,12 +13,13 @@ public class App {
     public static void main(String[] args) {
         File image = new File("src/FxT-CTKXwAYLgh-.jpg");
         Tesseract tesseract = new Tesseract();
-        tesseract.setDatapath("src/main/resources/tessdata");
-        tesseract.setLanguage("deu");
+        tesseract.setDatapath("tessdata");
+        tesseract.setLanguage("eng");
         tesseract.setPageSegMode(1);
-        tesseract.setOcrEngineMode(1);
+        tesseract.setOcrEngineMode(2);
         try {
             String result = tesseract.doOCR(image);
+            result.replace("1", "l");
             System.out.println(result);
         } catch (TesseractException e) {
             // TODO Auto-generated catch block
