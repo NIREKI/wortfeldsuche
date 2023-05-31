eb1873e46dff81385e7ac5a6b04a0553daffc7ad
Niklas <niklas.kinze@gmail.com>
2023-05-31 11:44:01 +0200

    Program is now able to check the array backwarts.

--- src/main/java/main/Solve.java
+++ src/main/java/main/Solve.java
@@ -1,13 +1,18 @@
 package main;
+
 import java.util.*;
 import java.io.*;
+
 /*
  * In this class, the values are put into an Array and all the values are scanned for english words.
  */
 public class Solve {
-    private static ArrayList<String> words;
-    public static void main(String[] args){
-        words =  new ArrayList<String>();
+    private static Set<String> wordSet;
+    private static ArrayList<String> words = new ArrayList<String>();
+
+    public static void main(String[] args) {
+        wordSet = new HashSet<>();
+        words = new ArrayList<String>();
         words.add("sEXbJJeNZbVDLUvFBppvh");
         words.add("mZsGyHvSsWTFABMlScFye");
         words.add("wovxtvWRqUddahluDbcfS");
@@ -24,51 +29,75 @@ public class Solve {
         words.add("TyuwwIgVLCaNRGclCqUix");
         words.add("PQDNsxynvUfxYybhRvaKI");
 
-        char[][] chars = new char[words.get(0).length()][words.size()];
+        char[][] chars = new char[words.size()][words.get(0).length()];
 
-        for(int i = 0; i < words.size(); i++){
+        for (int i = 0; i < words.size(); i++) {
             String a = words.get(i);
             chars[i] = a.toCharArray();
         }
-        //This part goes through the dictionary txt and saves it in a ArrayList
+
+        // This part goes through the dictionary txt and saves it in a ArrayList
         File f = new File("en_US.txt");
-        ArrayList<String> al = new ArrayList<String>();
         Scanner sc;
         try {
             sc = new Scanner(f);
-            while(sc.hasNext()){
+            while (sc.hasNext()) {
                 String a = sc.next();
-                if(a.contains("/")){
+                if (a.contains("/")) {
                     a = a.substring(0, a.indexOf("/", 0)).toLowerCase();
-                    
+
                 }
-                if(a.length() > 2)
-                    al.add(a);
+                if (a.length() > 2)
+                    wordSet.add(a);
             }
         } catch (FileNotFoundException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        //This part runs through every line of the array and checks from left to right for words. Needs to be turned around
-        //and also written that it displays words from top to bottom and bottom to top.
-        for(char[] a : chars){
-            for(int i = 1; i <= a.length; i++){
-                for(int j = 0; j < a.length; j++){
+        // This part runs through every line of the array and checks from left to right
+        // for words. Needs to be turned around
+        // and also written that it displays words from top to bottom and bottom to top.
+
+        for (char[] a : chars) {
+            for (int i = 1; i <= a.length; i++) {
+                for (int j = 0; j < a.length; j++) {
                     String wordToCheck = "";
-                    for(int k = j; k < i; k++){
+                    for (int k = j; k < i; k++) {
                         wordToCheck += a[k];
                     }
                     wordToCheck = wordToCheck.toLowerCase();
-                    if(wordToCheck.trim().length() > 0)
-                        if(al.contains(wordToCheck))
+                    if (wordToCheck.trim().length() > 0)
+                        if (wordSet.contains(wordToCheck))
                             System.out.println(wordToCheck);
-                        
+
                 }
             }
         }
-        
-        
+        /*
+         * Ability to check the array backwarts
+         */
+        System.out.println("Rückwärts");
+        for (char[] a : chars) {
+            for (int i = 1; i < a.length; i++) {
+                for (int j = a.length - 1; j >=0; j--) {
+                    String wordToCheck = "";
+                    if(j-i < 0)
+                        //Falls j - i kleiner als 0 ist, würde das Programm versuchen zu weit links zu schauen. Das geht natürlich nicht.
+                        break;
+                    for (int k = j; k >= j - i && k >= 0; k--) {
+                        wordToCheck += a[k];
+                    }
+                    
+                    wordToCheck = wordToCheck.toLowerCase();
+                    if (wordToCheck.trim().length() > 0)
+                        if (wordSet.contains(wordToCheck))
+                            System.out.println(wordToCheck);
+
+                }
+            }
+        }
+
+
     }
-            
 
 }
